c2b840f50f862202b9163c54f9d06633bc525336
kanchanCode <jkanchan2911@gmail.com>
2021-11-17 07:11:54 +0530

    added code for queue

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace StackAssignment
+{
+    class Program
+    {
+        public class Node
+        {
+            public static readonly object UC6;
+            public int data;
+            public int prev_node;
+            public Node next;
+
+            public Node(int data)
+            {
+                this.data = data;
+            }
+        }
+        static void Main(string[] args)
+        {
+            StackFunctions obj = new StackFunctions();
+            obj.Push(70);
+            obj.Push(30);
+            obj.Push(56);
+            obj.Display();
+            //obj.Peek();
+            //obj.Pop();
+            obj.isEmpty();
+            obj.Display();
+        }
+    }
+}
---
+++ StackFunctions.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static StackAssignment.Program;
+
+namespace StackAssignment
+{
+   
+    class StackFunctions
+    {
+        public Node top; 
+        
+
+        public StackFunctions()
+        {
+            this.top = null;
+        }
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("pushed element is "+value);
+        }
+
+        internal void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data+" ");
+                temp = temp.next;
+            }
+        }
+
+        internal void Peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Console.WriteLine(this.top.data+" is the data in the top of the stack");
+        }
+        internal void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Cannot Delete!! Stack is empty ");
+                return;
+            }
+           // Console.WriteLine(this.top.data+"");
+            this.top = this.top.next;
+            
+           
+        }
+        internal void isEmpty()
+        {
+            while (this.top != null)
+            {
+                Peek();
+                Pop();
+
+            }
+        }
+    }
+}
